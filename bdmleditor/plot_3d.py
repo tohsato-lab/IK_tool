667ea0b6de2ec770f422b6b7de0a75b1aea670ee
kazukazuinaina <ypa.ypa.ypa.t@gmail.com>
2020-10-10 19:47:05 +0900

    [add] 3D's slider

--- bdmleditor/plot_3d.py
+++ bdmleditor/plot_3d.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import h5py
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.widgets import Slider
 
 
 class Plot_3D:
@@ -21,8 +22,11 @@ class Plot_3D:
             self.y_data = np.append(self.y_data, data['y'].astype(np.float))
             self.z_data = np.append(self.z_data, data['z'].astype(np.float))
 
+        slider_pos = plt.axes([0.1, 0.01, 0.8, 0.03])
         self.points = self.ax.scatter3D(self.x_data, self.y_data, self.z_data, picker=10)
         self.fig.canvas.mpl_connect('pick_event', self.onclick)
+        threshold_slider = Slider(slider_pos, 'time', 0, 100, valinit=0, valstep=1, dragging=True)
+        threshold_slider.on_changed(self.update)
         plt.show()
 
     def onclick(self, event):
@@ -46,8 +50,21 @@ class Plot_3D:
         swap_data[ind][6] = update_value_z
 
         del f[self.object_id[0]]
-        # ここでバグ
         f.create_dataset(self.object_id[0], data=swap_data)
         data = f[self.object_id[0]]
         self.points = self.ax.scatter3D(data['x'].astype(np.float), data['y'].astype(np.float), data['z'].astype(np.float), picker=10)
+        self.fig.canvas.draw()
+
+    def update(self, slider_val):
+        from bdmleditor.bootstrap import data_load
+        self.object_id[0] = 'data/%s/object/0' %slider_val
+        # init
+        self.x_data, self.y_data, self.z_data = [], [], []
+        info = data_load(self.hdfpath, self.object_id)
+        for data in info[0][0]:
+            self.x_data = np.append(self.x_data, data['x'].astype(np.float))
+            self.y_data = np.append(self.y_data, data['y'].astype(np.float))
+            self.z_data = np.append(self.z_data, data['z'].astype(np.float))
+        self.points.remove()
+        self.points = self.ax.scatter(self.x_data, self.y_data, self.z_data,  s=1, picker=10)
         self.fig.canvas.draw()
